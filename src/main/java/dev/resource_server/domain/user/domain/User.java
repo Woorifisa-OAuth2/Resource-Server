67a4cef12d1b7b5a195c613efa15ec38c44574c5
yeonju73 <yeon703@naver.com>
2026-03-12 17:50:19 +0900

    feat: add user entity

---
+++ src/main/java/dev/resource_server/domain/user/domain/User.java
@@ -0,0 +1,29 @@
+package dev.resource_server.domain.user.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String age;
+
+    @Column(nullable = false)
+    private String gender;
+}
