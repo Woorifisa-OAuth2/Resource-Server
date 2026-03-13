6a2fcf9e65cf8d0f0d5736d52f4d903b38d3e506
yeonju73 <yeon703@naver.com>
2026-03-13 12:48:08 +0900

    fix: repository error

--- src/main/java/dev/resource_server/domain/user/repository/UserRepository.java
+++ src/main/java/dev/resource_server/domain/user/repository/UserRepository.java
@@ -9,7 +9,7 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    Optional<User> findByEmail(String email);
+    Optional<User> findByAuthId(String authId);
 
-    boolean existsByEmail(String email);
+    boolean existsByAuthId(String authId);
 }
